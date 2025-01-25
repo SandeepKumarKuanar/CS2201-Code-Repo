050376e823c8d49379f88446ab78984f130931f0
Sandeep Kumar Kuanar <sandeepkumarkuanar3@gmail.com>
2025-01-25 11:55:15 +0000

    Question no.9 added

--- Week-3/Question9.py
+++ Week-3/Question9.py
@@ -1,17 +1,11 @@
-#print("Hello, World!") ## this is real test
-#x = [1, 3, 2]
-#y = [1, 3, 2]
-x = [3, 2, 4]
-y = [3, 2, 4]
-y.sort()
-if x == y:
-    print("This is increasing monotone.")
-elif x != y:
-    for i in range(len(x)):
-        if i == 0:
-            continue
-        else:
-            if x[i] > x[i - 1] and x[i] < x[i + 1]:
-                print("It's not an monotone")
-                break
-    print("It's an decreasing monotone")
+#print("Hello World!") ##the real test
+x = [1, 2, 3, 4, 5]
+#x = [5, 4, 3, 2, 1]
+#x = [1, 2, 3, 2, 1]
+
+if x == sorted(x):
+    print("Monotonically increasing")
+elif x == sorted(x, reverse=True):
+    print("Monotonically decreasing")
+else:
+    print("Not monotonic")
